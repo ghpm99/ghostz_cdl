05149ccc0966da4b7053649ae392fc7020a19c3f
Guilherme H <ghpm99@hotmail.com>
2023-03-22 14:57:31 -0300

    Fix: check if league exist

--- overlay/views.py
+++ overlay/views.py
@@ -230,7 +230,7 @@ def import_json(request, user):
             date=overlay_data['Data'],
             hour=overlay_data['Horario'],
             modality=overlay_data['Modalidade'],
-            league=overlay_data['LIGA'] if overlay_data['LIGA'] else 'LIVERTO'
+            league=overlay_data['LIGA'] if overlay_data.get('LIGA') else 'LIVERTO'
         )
         overlay.save()
 
