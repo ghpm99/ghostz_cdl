dd192e760fd6eadda0a1df211f29e5979d72d241
Guilherme H <ghpm99@gmail.com>
2023-09-06 00:14:38 -0300

    Feat: Implements oauth callback view

--- youtube_overlay/urls.py
+++ youtube_overlay/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auth', views.get_oauth_token, name='get_oauth_token'),
+    path('auth/', views.get_oauth_token, name='get_oauth_token'),
+    path('oauth2callback/', views.oauth2_callback, name='oauth_callback'),
 ]
